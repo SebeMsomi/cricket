dd47f28aee76959380bc89a907f2106974b219bd
216028604 <zenandesebe@gmail.com>
2019-04-25 09:38:35 +0200

    sebe

--- src/main/java/za/ac/cput/Cricket/repos/CricketRepos.java
+++ src/main/java/za/ac/cput/Cricket/repos/CricketRepos.java
@@ -5,10 +5,10 @@ import za.ac.cput.Cricket.domain.Cricket;
 import java.util.Set;
 
 public interface CricketRepos extends Repo<Cricket, String> {
-    Cricket create(Cricket cricket);
+   /* Cricket create(Cricket cricket);
     Cricket update(Cricket cricket);
     void delete(Cricket cricket);
-    Cricket read(String cricketId);
+    Cricket read(String cricketId);*/
 
     Set<Cricket> getAll();
 }
---
+++ src/main/java/za/ac/cput/Cricket/repos/impl/DataBaseImp.java
@@ -0,0 +1,37 @@
+package za.ac.cput.Cricket.repos.impl;
+
+import za.ac.cput.Cricket.domain.Cricket;
+import za.ac.cput.Cricket.repos.CricketRepos;
+import java.util.Set;
+
+public class DataBaseImp implements CricketRepos {
+    @Override
+    public Set<Cricket> getAll()
+    {
+        return null;
+    }
+
+    @Override
+    public Cricket create(Cricket cricket)
+    {
+        return null;
+    }
+
+    @Override
+    public Cricket update(Cricket cricket)
+    {
+        return null;
+    }
+
+    @Override
+    public void delete(String s) {
+
+    }
+
+    @Override
+    public Cricket read(Cricket cricket)
+    {
+        return null;
+    }
+
+}
