edd3ab8a279ff136faf0d6967833ce24f5f3cce5
216028604 <zenandesebe@gmail.com>
2019-04-25 09:48:48 +0200

    sebe

--- src/main/java/za/ac/cput/Cricket/domain/Break.java
+++ src/main/java/za/ac/cput/Cricket/domain/Break.java
@@ -1,4 +1,46 @@
 package za.ac.cput.Cricket.domain;
 
+import java.util.Objects;
+
 public class Break {
+    private String type;
+
+    private Break(){}
+
+    public String getType() {
+        return type;
+    }
+
+    private Break(Builder builder) {
+
+
+        this.type = builder.type;
+
+    }
+    public static class Builder
+    {
+        private String type;
+
+        public Break.Builder type(String type){
+            this.type = type;
+            return this;
+        }
+
+        public Break build(){
+            return new Break(this);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Break abreak = (Break) o;
+        return type.equals(abreak.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type);
+    }
 }
---
+++ src/main/java/za/ac/cput/Cricket/util/BreakUtil.java
@@ -0,0 +1,9 @@
+package za.ac.cput.Cricket.util;
+
+import java.util.UUID;
+
+public class BreakUtil {
+    public static String genes(){
+        return UUID.randomUUID().toString();
+    }
+}
