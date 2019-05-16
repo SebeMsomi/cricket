d45f7aadffa5f8989545c4fd95b46e154052a1e2
Sebe <zenandesebe@gmail.com>
2019-05-17 00:57:35 +0200

    just added CRUD

--- src/test/java/za/ac/cput/Cricket/service/cricket/cricket/impl/CricketServiceImpTest.java
+++ src/test/java/za/ac/cput/Cricket/service/cricket/cricket/impl/CricketServiceImpTest.java
@@ -1,9 +1,7 @@
 package za.ac.cput.Cricket.service.cricket.cricket.impl;
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
+import org.junit.runners.MethodSorters;
 import za.ac.cput.Cricket.domain.Cricket;
 import za.ac.cput.Cricket.factory.CricketFactory;
 import za.ac.cput.Cricket.repos.impl.CricketRepositoryImp;
@@ -12,7 +10,54 @@ import za.ac.cput.Cricket.repos.impl.CricketRepositoryImp;
 import java.util.Set;
 
 import static org.junit.Assert.*;
-
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class CricketServiceImpTest {
+        private CricketRepositoryImp repository;
+        private Cricket cricket;
 
+    private Cricket getSaved(){
+        return this.repository.getAll().iterator().next();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        this.repository = (CricketRepositoryImp) CricketRepositoryImp.getRepository();
+        this.cricket = CricketFactory.getCricket("Protea", "India");
+    }
+    @Test
+    public void a_create() {
+        Cricket created = this.repository.create(this.cricket);
+        System.out.println("In create, created = " + created);
+        Assert.assertNotNull(created);
+        Assert.assertSame(created, this.cricket);
+    }
+    @Test
+    public void c_update() {
+        String newCricket = "Application Development Theory 3";
+        Cricket updated = new Cricket.Builder().copy(getSaved()).courseName(newCricket).build();
+        System.out.println("In update, updated = " + updated);
+        this.repository.update(updated);
+        Assert.assertSame(newCricket, updated.getCricketId());
+    }
+
+    @Test
+    public void e_delete() {
+        Cricket saved = getSaved();
+        this.repository.delete(saved.getCricketId());
+        d_getAll();
+    }
+
+    @Test
+    public void b_read() {
+        Cricket saved = getSaved();
+        Cricket read = this.repository.read(saved.getCricketId());
+        System.out.println("In read, read = "+ read);
+        Assert.assertSame(read, saved);
+    }
+
+    @Test
+    public void d_getAll() {
+        Set<Cricket> courses = this.repository.getAll();
+        System.out.println("In getall, all = " + courses);
+    }
 }
